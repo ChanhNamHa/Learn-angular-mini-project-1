b73b57f632aff896f6dd386bcce4ef7cba433e9a
hcnam <hcnam@tma.com.vn>
2023-12-13 09:12:30 +0700

    remove some lines

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -1,23 +1,13 @@
 import { useState } from "react";
 
 const Header = () => {
-    const [name, setName] = useState<string>('Ha Chanh Nam');
-    const UpdateName = () => {
-    var random = Math.round(Math.random()*100);
-    if(random % 2 == 0){
-      setName(name.toUpperCase());
-      console.log(random,"Uppercase Updated")
-    }else{
-      setName(name.toLowerCase());
-      console.log(random,"Lowercase Updated")
-    }
-  }
-    return (
-        <header className="App-header">
-            <h3>App Mini Game</h3>
-            <p>Design by {name}</p>
-            {/* <button onClick={UpdateName}>Click me</button> */}
-        </header>
-    )
-} ;
+  const [name, setName] = useState<string>("Ha Chanh Nam");
+  return (
+    <header className="App-header">
+      <h3>App Mini Game</h3>
+      <p>Design by {name}</p>
+      {/* <button onClick={UpdateName}>Click me</button> */}
+    </header>
+  );
+};
 export default Header;
